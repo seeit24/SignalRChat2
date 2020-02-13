58a5a58261c0d6cb72783d90852dbe8cb91b9507
seeitlive <seeitlive@hotmail.com>
2020-02-13 18:23:54 -0500

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRChat2.Hubs
+{
+    public class ChatHub: Microsoft.AspNetCore.SignalR.Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
